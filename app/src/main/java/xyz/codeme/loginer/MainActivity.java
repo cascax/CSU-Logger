85bcb42eef3f256264852146ab1198fb3927f0a6
MaCheng <mcxr4299@gmail.com>
2015-05-11 13:22:54 +0800

    use ToolBar

--- app/src/main/java/xyz/codeme/loginer/MainActivity.java
+++ app/src/main/java/xyz/codeme/loginer/MainActivity.java
@@ -4,15 +4,22 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "LoginerMain";
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
 
         FragmentManager fragmentManager = getFragmentManager();
         Fragment loginFragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
